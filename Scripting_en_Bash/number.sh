70dd14d1f610b6d3f04a9d92a20e2737d4747223
alexispc00 <111553509+alexispc00@users.noreply.github.com>
2022-11-29 00:05:12 -0600

    Create number.sh

---
+++ Scripting_en_Bash/number.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Proporciona un numero para variable 1: " n1
+read -p "Proporciona un numero para variable 2: " n2
+read -p "Proporciona un numero para variable 3: " n3
+
+echo "Numero 1 - $n1"
+echo "Numero 1 - $n2"
+echo "Numero 1 - $n3"
