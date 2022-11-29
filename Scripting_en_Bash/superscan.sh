586888e34f54b5f4cb7b38a4510115ca5554b5b1
alexispc00 <111553509+alexispc00@users.noreply.github.com>
2022-11-29 10:06:22 -0600

    Update superscan.sh

--- Scripting_en_Bash/superscan.sh
+++ Scripting_en_Bash/superscan.sh
@@ -1,1 +1,19 @@
-ss
+#!/bin/bash
+#
+# Ejemplo de Menu en BASH
+#
+date
+    echo "---------------------------"
+    echo "   Menu Principal"
+    echo "---------------------------"
+    echo "1. Net Discover"
+    echo "2. Portscanv1"
+    echo "3. Welcome"
+    echo "4. Exit"
+read -p "Opción  [ 1 - 4 ] " c
+case $c in
+        1) $HOME/netdiscover.sh;;
+        2) $HOME/portscanv1.sh;;
+        3) $HOME/welcome.sh;;
+        4) echo "Bye!"; exit 0;;
+esac
